553bc081319bcb6037f42173642f0e31954c029e
JethroSama <kel73896@gmail.com>
2020-06-12 16:44:51 +0800

    ahh

--- app.js
+++ app.js
@@ -10,4 +10,4 @@ app.get("/about", (req, res)=>{
 app.get("*", (req, res)=>{
   res.send("<h1>This page is like your girlfriend, It doesn't exist</h1>");
 });
-app.listen(3000 || process.env.PORT, process.env.IP);
+app.listen(80);
